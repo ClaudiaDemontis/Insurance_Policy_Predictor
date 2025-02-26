889bf99ac300d7c02308e2d38136510042eb09ec
aurorab97 <aurorabetta.97@gmail.com>
2025-02-26 17:08:24 +0100

    Updated "tables.py"
    Connected to database (insurance_db) and creation of tables

--- Tables.py
+++ Tables.py
@@ -1,37 +1,63 @@
 # --- TABLES CREATION ---
+import mysql.connector
+from mysql.connector import Error
+
+def esegui_query(query):
+
+    try:
+        # Create connection
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="insurance_db"
+        )
+        if connection.is_connected():
+            cursor = connection.cursor()
+            cursor.execute(query)
+            connection.commit()
+            cursor.close()
+            print(f"Query eseguita con successo: {query}")
+    except Error as e:
+        print(f"Errore durante l'esecuzione della query: {e}")
+        return None
+    finally:
+        if connection.is_connected():
+            connection.close()
+
 
 # Table Personal_Info
-CREATE TABLE Personal_Info (
-    personal_id INT PRIMARY KEY,
+esegui_query("""CREATE TABLE IF NOT EXISTS Personal_Info (
+    personal_id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255),
     surname VARCHAR(255),
     birth_year INT,
     mail VARCHAR(255) UNIQUE
-);
+);""")
 
 # Table Regions
-CREATE TABLE Regions (
-    regional_id INT PRIMARY KEY,
+esegui_query("""CREATE TABLE IF NOT EXISTS Regions (
+    regional_id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255),
     population INT,
     crime_rate FLOAT,
     unemployment_rate FLOAT,
     avg_income FLOAT,
     extreme_weather_events INT
-);
+);""")
 
 # Table Policies
-CREATE TABLE Policies (
-    policy_id INT PRIMARY KEY,
+esegui_query("""CREATE TABLE IF NOT EXISTS Policies (
+    policy_id INT PRIMARY KEY AUTO_INCREMENT,
     charges FLOAT,
     start_date DATE,
     end_date DATE,
     status VARCHAR(50) CHECK (status IN ('active', 'expired'))
-);
+);""")
 
 # Table Clients
-CREATE TABLE Clients (
-    client_id INT PRIMARY KEY,
+esegui_query("""CREATE TABLE IF NOT EXISTS Clients (
+    client_id INT PRIMARY KEY AUTO_INCREMENT,
     age INT,
     sex VARCHAR(10) CHECK (sex IN ('female', 'male')),
     bmi FLOAT,
@@ -43,4 +69,4 @@ CREATE TABLE Clients (
     FOREIGN KEY (id_region) REFERENCES Regions(regional_id),
     FOREIGN KEY (id_policy) REFERENCES Policies(policy_id),
     FOREIGN KEY (id_personal) REFERENCES Personal_Info(personal_id)
-);
+);""")
