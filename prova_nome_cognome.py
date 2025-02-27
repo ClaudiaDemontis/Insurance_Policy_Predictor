4057733352cea5338fefc120aed2c44c98b48fea
aurorab97 <aurorabetta.97@gmail.com>
2025-02-27 15:07:07 +0100

    Update prova_nome_cognome
    Ho aggiunto anche birth_year e ho fatto la query per inserire i dati (a me funziona)

--- prova_nome_cognome.py
+++ prova_nome_cognome.py
@@ -1,9 +1,31 @@
-import mysql
 import csv
 from faker import Faker
+import mysql.connector
+from mysql.connector import Error
 
-def esegui_query_parametrizzata(query, parametri):
+def esegui_query(query):
     try:
+        # Creazione connessione
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="olympics_db"
+        )
+        if connection.is_connected():
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                connection.commit()
+                print(f"Query eseguita con successo: {query}")
+    except Error as e:
+        print(f"Errore durante l'esecuzione della query: {e}")
+    finally:
+        if 'connection' in locals() and connection.is_connected():
+            connection.close()
+
+def esegui_query_place(query, values):
+    try:
+        # Creazione connessione
         connection = mysql.connector.connect(
             host="localhost",
             user="root",
@@ -11,28 +33,15 @@ def esegui_query_parametrizzata(query, parametri):
             database="insurance_db"
         )
         if connection.is_connected():
-            cursor = connection.cursor()
-            cursor.execute(query, parametri)
-            connection.commit()
-            cursor.close()
-            print(f"Dati inseriti correttamente: {parametri}")
+            with connection.cursor() as cursor:
+                cursor.execute(query, values)
+                connection.commit()
+                print(f"Query eseguita con successo: {query}")
     except Error as e:
         print(f"Errore durante l'esecuzione della query: {e}")
     finally:
-        if connection.is_connected():
+        if 'connection' in locals() and connection.is_connected():
             connection.close()
-
-def esegui_query_parametrizzata_connection(query, parametri, connection):
-    try:
-        if connection.is_connected():
-            cursor = connection.cursor()
-            cursor.execute(query, parametri)
-            connection.commit()
-            cursor.close()
-            print(f"Dati inseriti correttamente: {parametri}")
-    except Error as e:
-        print(f"Errore durante l'esecuzione della query: {e}")
-
 
 with open("Insurance_cleaned.csv", encoding="utf-8") as f:
     dati = csv.reader(f)
@@ -56,16 +65,18 @@ with open("Insurance_cleaned.csv", encoding="utf-8") as f:
         # con questi dati creo le mail
         mail = f"{name.lower()}.{surname.lower()}@example.com"
 
+        # ora creo l'anno di nascita
+        age = float(riga[0])
+        birth_year = 2025 - age
+
         # aggiungo nome, cognome, mail nella lista data
-        data.append((name, surname, mail))
+        data.append((name, surname, birth_year, mail))
 
 print(data)
 
 
+query_persona_info = """
+INSERT INTO personal_info (name, surname, birth_year, mail) VALUES (%s, %s, %s, %s)"""
 
-
-
-
-
-# query_persona_info = """
-# INSERT INTO personal_info (name, surname, birth_year, mail) VALUES (%, %, %, %)"""
+for d in data:
+    esegui_query_place(query_persona_info, d)
