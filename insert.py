6154ab643bab20dca52c8dc6ed5e892183c219b3
ClaudiaDemontis <142052694+ClaudiaDemontis@users.noreply.github.com>
2025-02-27 12:33:47 +0100

    Update insert.py

--- insert.py
+++ insert.py
@@ -1,36 +1,19 @@
-import pandas as pd
-import mysql.connector
 import random
 from faker import Faker
-df = pd.read_csv('insurance_modified.csv')
-
 
 # Creare un'istanza di Faker
 fake = Faker()
 
-# Connessione al database MySQL
-conn = mysql.connector.connect(
-    host='localhost',
-    user='root',
-    password='',
-    database='insurance_db'
-)
-cursor = conn.cursor()
+# Import
+from funzioni_utili import esegui_query
 
-import pandas as pd
+# Inserimento dati nella tabella regions
+query_regions = """
+INSERT INTO Regions (name, population, crime_rate, unemployment_rate, avg_income, extreme_weather_events) VALUES 
+('Northeast', 57000000, 0.23, 3.72, 96859, 21.67),
+('Southeast', 101000000, 0.35, 3.70, 78320, 27.50),
+('Northwest', 15500000, 0.27, 3.80, 82624, 16.67),
+('Southwest', 53700000, 0.30, 4.45, 77453, 25.50);
+"""
 
-# Dati esemplificativi delle regioni negli Stati Uniti (sostituire con dati reali)
-regions_data = {
-    "name": ["Northeast", "Midwest", "South", "West"],
-    "population": [55900000, 68000000, 125000000, 78000000],  # Popolazione per area
-    "crime_rate": [4.5, 5.3, 6.8, 3.2],  # Tasso di criminalità per 100,000 abitanti (esempio)
-    "unemployment_rate": [5.3, 4.6, 6.1, 4.4],  # Tasso di disoccupazione (%)
-    "avg_income": [57000, 50000, 45000, 65000],  # Reddito medio (USD)
-    "extreme_weather_events": [3, 6, 7, 5]  # Eventi climatici estremi per anno (esempio)
-}
-
-# Creare un DataFrame con questi dati
-regions_df = pd.DataFrame(regions_data)
-
-# Visualizzare i dati
-print(regions_df)
+esegui_query(query_regions)
