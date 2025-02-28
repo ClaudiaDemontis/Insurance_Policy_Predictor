c78f58f2711c2e70ba33a39611d8da4e2408bea5
ClaudiaDemontis <142052694+ClaudiaDemontis@users.noreply.github.com>
2025-02-28 09:34:53 +0100

    Update insert_policies.py

--- insert_policies.py
+++ insert_policies.py
@@ -3,50 +3,47 @@
 # Imports
 import csv
 from faker import Faker
-import mysql.connector
-from mysql.connector import Error
-from funzioni_utili import esegui_query
 from funzioni_utili import esegui_query_parametrizzata
 import datetime
 
 # Inserimento dei dati nella tabella
-
-# lettura del csv
+# Lettura del csv
 with open("Insurance_cleaned.csv", encoding="utf-8") as f:
     dati = csv.reader(f)
+    # Salto la riga dell'intestazione
     next(dati)
 
     fake = Faker()
 
-    # creo una lista vuota policies
+    # Creo una lista vuota policies
     policies = []
 
-    # associo le righe del csv ai campi delle tabelle
+    # Associo le righe del csv ai campi delle tabelle
     for riga in dati:
-        age = riga[0]
-        sex = riga[1]
-        bmi = riga[2]
-        children = riga[3]
-        smoker = riga[4]
-        region = riga[5]
+        # age = riga[0]
+        # sex = riga[1]
+        # bmi = riga[2]
+        # children = riga[3]
+        # smoker = riga[4]
+        # region = riga[5]
         charges = riga[6]
 
-        # utilizzo fake.date_between per ottenere delle date del 2024
+        # Utilizzo fake.date_between per ottenere delle date del 2024
         start_date = fake.date_between(start_date=datetime.datetime(2024, 1, 1),
                                        end_date=datetime.datetime(2024, 12, 31))
-        # estraggo anno, mese e giorno
+        # Estraggo anno, mese e giorno
         year = start_date.year
         month = start_date.month
         day = start_date.day
-
+        
         try:
-            # sostituisco l'anno con 2005 per avere la data di scadenza della polizza
+            # sostituisco l'anno con 2025 per avere la data di scadenza della polizza
             end_date = start_date.replace(year=2025)
         except ValueError as e:
             # ma devo impostare una data valida nel caso ci fosse un errore
             print(f"Errore nella data: {e}, impostata una data valida")
             # una data valida potrebbe essere il 2025-12-31
-            end_date = datetime.date(2025, 12, 31)
+            end_date = datetime.date(2025, 3, 1)
 
         # determino se la polizza è attiva o scaduta
         if end_date < datetime.date.today():
