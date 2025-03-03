273fcd6e89d6ce136d4579a17874402284b0e5e9
fbassa <fbassa98@gmail.com>
2025-03-03 15:15:32 +0000

    flask/webapp
    create html

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template
+import mysql.connector
+from mysql.connector import Error
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+# def index():
+#     conn = mysql.connector.connect(
+#         host="localhost",
+#         user="root",  # Inserisci il tuo nome utente
+#         password="",  # Inserisci la tua password
+#         database="vgsales"  # Inserisci il nome del tuo database
+#     )
+#     cursor = conn.cursor(dictionary= True)
+#     q = """SELECT gioco.nome_gioco, gioco.anno, piattaforma.nome_piattaforma
+#         FROM gioco
+#         INNER JOIN piattaforma ON gioco.piattaforma_id = piattaforma.id_piattaforma"""
+#     cursor.execute(q)
+#     risultato = cursor.fetchall()
+#     cursor.close()
+#     conn.close()
+#     return render_template("index.html", lista_vg = risultato)
+#
+#
+# #rotta parametrizzata
+# # @app.route("/sport/<selected_sport>")
+# # def sport_players(selected_sport):
+# #     lista_giocatori = []
+# #     for elem in players:
+# #         if elem["sport"] == selected_sport.lower():
+# #             lista_giocatori.append(elem)
+# #     #return lista_giocatori
+# #     return render_template("index.html", players = lista_giocatori)
+#
+#
+
+if __name__ == "__main__":
+    app.run(debug=True)
