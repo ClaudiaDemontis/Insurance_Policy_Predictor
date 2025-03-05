d63f15c7f96a6e629c3eeacbaa0020a1624dce8a
fbassa <fbassa98@gmail.com>
2025-03-05 10:05:48 +0000

    Update machine_learning.py

--- machine_learning.py
+++ machine_learning.py
@@ -2,9 +2,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import math
 
 # Carica il dataset
 df = pd.read_csv('Insurance_cleaned.csv')
+
+#converto le variabili non numeriche
+df = pd.get_dummies(df, columns=['sex', 'smoker', 'region'], drop_first=True)
 
 # Esplora i primi record del dataset
 print(df.head())
@@ -21,39 +25,59 @@ plt.show()
 # Costruzione del modello di regressione lineare
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler
 
 # Seleziona le variabili indipendenti e dipendenti
 X = df.drop(columns=['charges'])
 y = df['charges']
 
+#grafico iniziale
+
+
 # Suddivisione in training e test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+# Feature scaling
+scaler = StandardScaler()
+# we are going to scale ONLY the features (i.e. the X) and NOT the y!
+X_train_scaled = scaler.fit_transform(X_train) # fitting to X_train and transforming them
+X_test_scaled = scaler.transform(X_test) # transforming X_test. DO NOT FIT THEM!
+
+# Visualizing data after scaling
+print(f"\n-- AFTER SCALING -- X_train:\n{X_train_scaled[:5]}")
+print(f"\n-- AFTER SCALING -- y_train:\n{y_train[:5]}")
+print(f"\n-- AFTER SCALING -- X_test:\n{X_test_scaled[:5]}")
+print(f"\n-- AFTER SCALING -- y_test:\n{y_test[:5]}")
+
 # Modello di regressione lineare
 model = LinearRegression()
-model.fit(X_train, y_train)
+model.fit(X_train_scaled, y_train)
+
+#coefficiente del modello
+coeff_df = pd.DataFrame(model.coef_, X.columns, columns=['Coefficient'])
+print(coeff_df)
 
 # Predizioni
-y_pred = model.predict(X_test)
+y_pred = model.predict(X_test_scaled)
 
 # Valutazione del modello
 print('MSE:', mean_squared_error(y_test, y_pred))
-print('R^2:', r2_score(y_test, y_pred))
+print('MAE:', mean_absolute_error(y_test, y_pred))
+print('RMSE:', math.sqrt(mean_squared_error(y_test, y_pred)))
+
+# residuals = y_test - model.predict(X_test)
+# plt.figure(figsize=(8,6))
+# sns.histplot(residuals, bins=30, kde=True)
+# plt.xlabel("Errore della previsione")
+# plt.ylabel("Frequenza")
+# plt.title("Distribuzione degli errori della regressione")
+# plt.show()
+#
+# 
+#
+#
+# #sns.pairplot(df,diag_kind='kde')
 
 
-# 1. Analisi Età-Costo
-import matplotlib.pyplot as plt
-import seaborn as sns
 
-# Grafico di dispersione tra età e costo
-plt.figure(figsize=(10, 6))
-sns.scatterplot(x='age', y='charges', data=df)
-plt.title('Relazione tra Età e Costo della Polizza')
-plt.xlabel('Età')
-plt.ylabel('Costo della Polizza')
-plt.show()
-
-# Calcolare la correlazione tra età e costo
-correlation_age_cost = df['age'].corr(df['charges'])
-print(f'Correlazione tra Età e Costo: {correlation_age_cost}')
